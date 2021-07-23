e17382ccfacd46ee54efd06379bd37d6aa50d721
DG <dg@email.com>
2021-07-23 13:49:29 +0530

    first version done

---
+++ src/index.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+import { init, FieldExtensionSDK } from '@contentful/app-sdk';
+import '@contentful/forma-36-react-components/dist/styles.css';
+import './index.css';
+import { Heading, SectionHeading } from '@contentful/forma-36-react-components';
+import { FieldAPI } from '@contentful/app-sdk';
+
+interface AppProps {
+  sdk: FieldExtensionSDK;
+}
+
+const App: React.FC<AppProps> = (props: AppProps) => {
+  const parameter: any = props.sdk.parameters.instance;
+
+  
+  const extendedField = (props.sdk.field as any) as FieldAPI;
+
+  const sdk = props.sdk;
+
+  extendedField.onSchemaErrorsChanged = () => () => null;
+  extendedField.setInvalid = () => null;
+  extendedField.locale = sdk.locales.default;
+
+  const fieldDetails = sdk.contentType.fields.find(({ id }) => id === extendedField.id);
+  const fieldEditorInterface = sdk.editor.editorInterface?.controls?.find(
+    ({ fieldId }) => fieldId === extendedField.id
+  );
+
+  console.log("*****", fieldEditorInterface?.settings)
+  
+  return (
+    <div className="container">
+      <SectionHeading element="h1" style={{fontSize: "1rem", paddingBottom: "0rem"}}>{parameter.message}</SectionHeading>
+    </div>
+  );
+};
+
+init((sdk: FieldExtensionSDK) => {
+  sdk.window.startAutoResizer();
+  render(<App sdk={sdk as FieldExtensionSDK} />, document.getElementById('root'));
+});
+
+/**
+ * By default, iframe of the app is fully reloaded on every save of a source file.
+ * If you want to use HMR (hot module reload) instead of full reload, uncomment the following lines
+ */
+// if (module.hot) {
+//   module.hot.accept();
+// }
